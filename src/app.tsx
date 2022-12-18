157d7da3a384fb7421035aaf6a2ff91a23a4ecf2
Rauan de Santana Rosa <rauanoronha@hotmail.com>
2022-12-18 17:51:03 -0300

    Finalizando Projeto

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,69 @@
 import React from 'react';
 import LogoDio from './assets/image/logo_dio.svg';
 import { Column, Container, Row } from './style/global';
-import { Button, Content, ImageLogo, Input } from './style/app';
+import { Button, Content, Form, ImageLogo, Input, InputError } from './style/app';
+import { useForm } from 'react-hook-form';
 
 const App = () => {
+  const { 
+    register, 
+    handleSubmit,
+    formState: {errors, isValid}
+  } = useForm({mode: "onChange", reValidateMode: "onChange"});
+
+  const onSubmit = (data: any) => console.log(data);
+
+  const emailError = (error: any) => {
+    const valueError = error?.message?.toString();
+
+    if (valueError === undefined) {
+      return "";
+    } else if (valueError === "empty") {
+      return "Campo obrigatório";
+    } else if (valueError === "") {
+      return "E-mail invalido";
+    } else {
+      return "";
+    }
+  }
+
+  const passwordError = (error: any) => {
+    const valueError = error?.message?.toString();
+
+    if (valueError === undefined) {
+      return "";
+    } else if (valueError === "empty") {
+      return "Campo obrigatório";
+    } else if (valueError === "") {
+      return "Minimo de 6 caracteres";
+    } else {
+      return "";
+    }
+  }
+
   return (
     <Container>
       <Column alignContent='center'>
         <Row height='auto' alignContent='center'>
           <Content>
             <ImageLogo src={LogoDio} />
-            <Input placeholder='E-mail' type='email' />
-            <Input placeholder='Password' type='password' />
-            <Button type='button'>Entrar</Button>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              {/* Input E-mail */}
+              <Input placeholder='E-mail' type='email' {...register('email', { 
+                pattern: /^([\w\.-_]+)@+[\w]+((\.+\w{2,3}){1,2})$/g,
+                required: "empty",
+              })} aria-invalid={errors.email ? "true" : "false"} />
+              {/* Menssagem de Erro E-mail */}
+              <InputError role="alert">{ emailError(errors.email) }</InputError>
+              {/* Input Password */}
+              <Input placeholder='Password' type='password' {...register('password', {
+                minLength: 6, 
+                required: "empty",
+              })} aria-invalid={errors.password ? "true" : "false"} />
+              {/* Menssagem de Erro Password */}
+              <InputError role="alert">{ passwordError(errors.password) }</InputError>
+              <Button disabled={!isValid} type='submit'>Entrar</Button>
+            </Form>
           </Content>
         </Row>
       </Column>
--- src/style/app.ts
+++ src/style/app.ts
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { AppTypes } from "./app_types";
 
 export const Content = styled.div`
     width: 50%;
@@ -21,23 +22,41 @@ export const ImageLogo = styled.img`
     margin-bottom: 40px;
 `;
 
+export const Form = styled.form`
+    width: 100%;
+    
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
 export const Input = styled.input`
     width: 80%;
     height: 45px;
-    margin-bottom: 15px;
     padding: 0px 10px;
     border: 1px solid #000;
     border-radius: 0.5rem;
     background-color: #161220;
 `;
 
-export const Button = styled.button`
+export const InputError = styled.p`
+    width: 75%;
+    margin-top: 5px;
+    margin-bottom: 15px;
+    font-size: 14px;
+    font-weight: 500;
+    color: #EA3737;
+`;
+
+export const Button = styled.button<AppTypes>`
     position: relative;
     width: calc(80% - 10px);
     height: 40px;
     margin-top: 15px;
     margin-bottom: 25px;
     padding: 0px 10px;
+    font-weight: 700;
     border-radius: 2rem;
     background-color: #E4105D;
     cursor: pointer;
@@ -55,4 +74,12 @@ export const Button = styled.button`
     
     &:hover { filter: brightness(1.05); }
     &:active { filter: brightness(1.2); }
+
+
+    ${({disabled}) => (disabled === true) && css`
+        opacity: 0.5;
+        cursor: default;
+        &:hover { filter: brightness(1); }
+        &:active { filter: brightness(1); }
+    `}
 `;
